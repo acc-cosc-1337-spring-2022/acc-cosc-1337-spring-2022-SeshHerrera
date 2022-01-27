423cfa3e599c6d719926bb0931a7b7097a3d52bb
SeshHerrera <sesh.herrera@g.austincc.edu>
2022-01-27 14:26:55 -0600

    include types.h

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,12 @@
 //Write the include statement for types.h here
-
+#include <types.h>
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include <iostream>
 
 
 int main() 
 {
-
+	std::cout << "hello";
 	return 0;
 }
 
